39262287ec9253da6dbc7e5f8730051756ad86a1
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-11-16 01:18:07 -0200

    Update default configurations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,23 +32,6 @@
                     <h1 class="text-grey-darker text-center font-hairline tracking-wide text-7xl mb-6">
                         {{ config('app.name', 'Laravel') }}
                     </h1>
-                    <ul class="list-reset">
-                        <li class="inline pr-8">
-                            <a href="https://laravel.com/docs" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Documentation">Documentation</a>
-                        </li>
-                        <li class="inline pr-8">
-                            <a href="https://laracasts.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Laracasts">Laracasts</a>
-                        </li>
-                        <li class="inline pr-8">
-                            <a href="https://laravel-news.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="News">News</a>
-                        </li>
-                        <li class="inline pr-8">
-                            <a href="https://forge.laravel.com" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="Forge">Forge</a>
-                        </li>
-                        <li class="inline pr-8">
-                            <a href="https://github.com/laravel/laravel" class="no-underline hover:underline text-sm font-normal text-brand-dark uppercase" title="GitHub">GitHub</a>
-                        </li>
-                    </ul>
                 </div>
             </div>
         </div>
