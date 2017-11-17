c6b2bfb71d45ea1b2f9b3c36740926a38c8d221e
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-11-17 00:07:26 -0200

    Little design change on time

--- resources/views/layouts/topics.blade.php
+++ resources/views/layouts/topics.blade.php
@@ -4,14 +4,14 @@
             <a href="{{ route('topic', $topic) }}" class="block p-4 bg-indigo border-b border-indigo-lighest no-underline text-white">
                 <div class="flex justify-between">
                     {{ $topic->title }}
-                    <span>{{ $topic->time }}</span>
+                    <span class="font-bold">{{ $topic->time }}</span>
                 </div>
            </a>
         @else
             <a href="{{ route('topic', $topic) }}" class="block p-4 border-b border-indigo-lighest no-underline text-grey-dark hover:text-grey-darkest">
                 <div class="flex justify-between">
                     {{ $topic->title }}
-                    <span>{{ $topic->time }}</span>
+                    <span class="font-bold">{{ $topic->time }}</span>
                 </div>
             </a>
         @endif
