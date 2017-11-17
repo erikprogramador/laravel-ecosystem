5e2331338c6f3f219e3050a5f644d02b80988dc1
Erik Vanderlei Fernandes <erik.vanderlei.programador@outlook.com>
2017-11-16 23:14:52 -0200

    About

--- database/seeds/AuthorTableSeeder.php
+++ database/seeds/AuthorTableSeeder.php
@@ -24,14 +24,14 @@ class AuthorTableSeeder extends Seeder
             'name' => 'Sales City',
             'company' => 'Sales City',
             'link' => 'https://salescity.com.br',
-            'description' => 'O Sales City é um sistema com foco em organizar e compartilhar informação entre membros da sua equipe, para o processo de marketing e vendas. Pois quando a informação é de fácil acesso pelos membros de sua equipe o processo fica muito mais rápido e eficaz.',
+            'description' => 'Sistema de gerenciamento de clientes.',
         ]);
 
         $author->projects()->create([
             'name' => 'SimulaImob',
             'company' => 'Adalov',
             'link' => 'https://simulaimob.com.br',
-            'description' => 'O SimulaImob é uma plataforma de simulação de valores de financiamento imobiliário.',
+            'description' => 'Plataforma de simulação de financiamento imobiliario.',
         ]);
     }
 }
--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -1,28 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-    <div>
-        <img src="{{ asset('images/avatar.jpeg') }}" alt="{{ $author->name }}" title="{{ $author->name }}"/>
-        <div>
-            <h1>{{ $author->name }}</h1>
-            <div>
-                <span>{{ $author->twitter_tag }}</span>
-                <span>{{ $author->github_tag }}</span>
+    <div class="text-center">
+        <img src="{{ asset('images/avatar.jpeg') }}" class="rounded w-1/3" alt="{{ $author->name }}" title="{{ $author->name }}"/>
+        <div class="mt-4">
+            <h1 class="text-grey-darkest">{{ $author->name }}</h1>
+            <div class="mt-2 text-grey-darker">
+                <a class="no-underline bg-grey-light py-1 px-3 text-md text-grey-darkest hover:bg-grey hover:text-white inline-block mx-2" href="{{ $author->twitter }}">
+                    <i class="fa fa-twitter"></i>
+                    {{ $author->twitter_tag }}
+                </a>
+                <a class="no-underline bg-grey-light py-1 px-3 text-md text-grey-darkest hover:bg-grey hover:text-white inline-block mx-2" href="{{ $author->github }}">
+                    <i class="fa fa-github"></i>
+                    {{ $author->github_tag }}
+                </a>
             </div>
         </div>
+    </div>
 
-        <div>
-            <div>
-                @foreach ($author->projects as $project)
+    <div class="-mx-4 mt-8">
+        <div class="flex">
+            @foreach ($author->projects as $project)
+                <div class="w-1/2 mx-4 bg-white shadow-md p-4">
                     <h2>
-                        <a href="{{ $project->link }}">
+                        <a class="no-underline text-grey-darkest" href="{{ $project->link }}">
                             {{ $project->name }}
                         </a>
-                        <small>{{ $project->company }}</small>
+                        <small class="ml-2 text-grey-darkest">({{ $project->company }})</small>
                     </h2>
-                    <div>{{ $project->description }}</div>
-                @endforeach
-            </div>
+                    <div class="mt-4 text-grey-darkest leading-normal">{!! $project->description !!}</div>
+                </div>
+            @endforeach
         </div>
     </div>
 @endsection
